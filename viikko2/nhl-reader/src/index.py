9847d66bd6084ce4e118a97d6e0cffaa42656ad0
shadowlegend551 <oskari.j.lappalainen@gmail.com>
2025-11-10 17:02:40 +0200

    Finished exercise 5 of week2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,21 +1,12 @@
-import requests
-from player import Player
+from player import PlayerReader, PlayerStats 
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        if player_dict['nationality'] == 'FIN':
-            player = Player(player_dict)
-            players.append(player)
-
-    print("Oliot:")
+    
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scores_by_nationality('FIN')
 
     for player in players:
         print(player)
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,6 @@
+from requests import get
+
+
 class Player:
     def __init__(self, dict):
         self.name           = dict['name']
@@ -8,6 +11,29 @@ class Player:
         self.games          = dict['games']
         self.id             = dict['id']
         
-
     def __str__(self):
         return self.name
+
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        return [Player(player_dict) for player_dict in get(self.url).json()]
+
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scores_by_nationality(self, nationality):
+        chosen_players = []
+        for player in self.players:
+            if player.nationality == nationality:
+                chosen_players.append(player)
+        return sorted(chosen_players,
+                      key=lambda p: p.assists+p.goals,
+                      reverse=True)
+
+
